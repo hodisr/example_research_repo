fe3bbc8c37f80dfc4266fbcad7c20883dd04b8fe
Hod Israeli <hodisraeli@gmail.com>
2019-08-12 21:54:21 +0300

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -10,7 +10,7 @@ def test_model():
     data = DummyProcessor.load_from_files(features_path=sample_features_path, targets_path=sample_targets_path)
     model = load_model(sample_model_path)
     score = train_model(data, model)
-    return score < MIN_SCORE_THRESHOLD
+    return score > MIN_SCORE_THRESHOLD
 
 
 if __name__ == '__main__':
